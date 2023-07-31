676de80a5f3d8d63ac8a71c95883171e68cd1a57
Abebayehu Tefera <abebayehutefera@gmail.com>
2023-07-31 05:42:45 -0700

    Update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,14 +76,31 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """retrieve one object
         """
-        objects = list(self.all(cls).values())
-        for object in objects:
-            if object.id == id:
-                return (object)
+        Returns the object based on the class and its ID, or None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+
+        return None
 
     def count(self, cls=None):
-        """count the number of objects in storage"""
-        objects = list(self.all(cls).values())
-        return(len(objects))
+        """
+        counts the number of objects in storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
