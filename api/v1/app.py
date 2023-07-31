b3abe547f20a16df220c5a0267fe61547d2f9a18
pgk95 <pgkkalu@gmail.com>
2023-07-31 10:07:01 -0700

    task 5 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Closes the storage session."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
